5c76d8a267283d191978ea131df2f4a7a838a629
Siuon Tom <sotom@sotom.tw>
2020-05-05 21:31:33 +0800

    add day 4, Number Complement, not complete yet :(

---
+++ 004_Number_Complement/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _004_Number_Complement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Given a positive integer, output its complement number. 
+               The complement strategy is to flip the bits of its binary representation.
+  
+             * Example 1:
+             * Input: 5
+             * Output: 2
+             * Explanation: The binary representation of 5 is 101 (no leading zero bits), 
+               and its complement is 010. So you need to output 2.
+             
+             * Example 2:
+             * Input: 1
+             * Output: 0
+             * Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+              
+             * Note:
+             * 1. The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+             * 2. You could assume no leading zero bit in the integer’s binary representation.
+             * 3. This question is the same as 1009: https://leetcode.com/problems/complement-of-base-10-integer/
+            */
+
+            var num = 5;
+
+            var solution = new Solution();
+
+            Console.WriteLine(solution.FindComplement(num) == 2);
+        }
+    }
+
+    public class Solution
+    {
+        public int FindComplement(int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
