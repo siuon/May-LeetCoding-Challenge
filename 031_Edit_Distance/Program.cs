b4d9436536c066c98408b3285c2b3866fb3a19f2
Siuon Tom <sotom@sotom.tw>
2020-06-01 20:50:48 +0800

    add day 31, Edit Distance, not complete yet :(

---
+++ 031_Edit_Distance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _031_Edit_Distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+             * You have the following 3 operations permitted on a word:
+             * Insert a character
+             * Delete a character
+             * Replace a character
+
+             * Example 1:
+             * Input: word1 = "horse", word2 = "ros"
+             * Output: 3
+             * Explanation: 
+             * horse -> rorse (replace 'h' with 'r')
+             * rorse -> rose (remove 'r')
+             * rose -> ros (remove 'e')
+             */
+
+            var word1 = "horse";
+            var word2 = "ros";
+
+            var solution = new Solution();
+
+            //MessageBox.Show("test");
+
+            Console.WriteLine(solution.MinDistance(word1, word2) == 3);
+        }
+    }
+
+    public class Solution
+    {
+        public int MinDistance(string word1, string word2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
